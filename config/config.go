99505abb611602ffbd3ac86ec5652e2329494966
yanuar <yanuar.nurcahyo@stockbit.com>
2022-06-02 11:32:57 +0700

    update config structure

--- config/config.go
+++ config/config.go
@@ -42,8 +42,59 @@ var (
 	ReadDBPort int
 )
 
+type Config struct {
+	// Development env checking, this env for debug purpose
+	Development string
+
+	// HTTPPort config
+	HTTPPort uint16
+
+	// GRPCPort config
+	GRPCPort string
+
+	WriteDB ConfigWriteDB
+
+	ReadDB ConfigReadDB
+}
+
 // Load function will load all config from environment variable
-func Load() error {
+func Load() (Config, error) {
+
+	var (
+		c   Config
+		err error
+	)
+
+	// load .env
+	err = dotenv.Load(".env")
+	if err != nil {
+		return c, err
+	}
+
+	err = c.setDevelopmentMode()
+	if err != nil {
+		return c, err
+	}
+
+	err = c.setHTTPPort()
+	if err != nil {
+		return c, err
+	}
+
+	err = c.setReadDB()
+	if err != nil {
+		return c, err
+	}
+
+	err = c.setWriteDB()
+	if err != nil {
+		return c, err
+	}
+
+	return c, err
+}
+
+func (c *Config) setDevelopmentMode() error {
 	// load .env
 	err := dotenv.Load(".env")
 	if err != nil {
@@ -55,11 +106,11 @@ func Load() error {
 		return errors.New("DEVELOPMENT env is not loaded")
 	}
 
-	// set Development
-	Development = development
+	c.Development = development
+	return nil
+}
 
-	// ------------------------------------
-
+func (c *Config) setHTTPPort() error {
 	httpPortStr, ok := os.LookupEnv("HTTP_PORT")
 	if !ok {
 		return errors.New("HTTP_PORT env is not loaded")
@@ -71,15 +122,18 @@ func Load() error {
 	}
 
 	// set http port
-	HTTPPort = uint16(httpPort)
+	c.HTTPPort = uint16(httpPort)
+	return nil
+}
 
+func (c *Config) setWriteDB() error {
 	writeDBHost, ok := os.LookupEnv("WRITE_DB_HOST")
 	if !ok {
 		return errors.New("WRITE_DB_HOST env is not loaded")
 	}
 
 	// set WriteDBHost
-	WriteDBHost = writeDBHost
+	c.WriteDB.Host = writeDBHost
 
 	writeDBName, ok := os.LookupEnv("WRITE_DB_NAME")
 	if !ok {
@@ -87,7 +141,7 @@ func Load() error {
 	}
 
 	// set WriteDBName
-	WriteDBName = writeDBName
+	c.WriteDB.Name = writeDBName
 
 	writeDBUser, ok := os.LookupEnv("WRITE_DB_USER")
 	if !ok {
@@ -95,7 +149,7 @@ func Load() error {
 	}
 
 	// set WriteDBUser
-	WriteDBUser = writeDBUser
+	c.WriteDB.User = writeDBUser
 
 	writeDBPassword, ok := os.LookupEnv("WRITE_DB_PASSWORD")
 	if !ok {
@@ -103,7 +157,7 @@ func Load() error {
 	}
 
 	// set WriteDBPassword
-	WriteDBPassword = writeDBPassword
+	c.WriteDB.Password = writeDBPassword
 
 	writeDBPort, ok := os.LookupEnv("WRITE_DB_PORT")
 	if !ok {
@@ -111,7 +165,51 @@ func Load() error {
 	}
 
 	// set WriteDBPort
-	WriteDBPort, _ = strconv.Atoi(writeDBPort)
-	// ------------------------------------
+	c.WriteDB.Port, _ = strconv.Atoi(writeDBPort)
+
+	return nil
+}
+
+func (c *Config) setReadDB() error {
+	readDBHost, ok := os.LookupEnv("READ_DB_HOST")
+	if !ok {
+		return errors.New("READ_DB_HOST env is not loaded")
+	}
+
+	// set ReadDBHost
+	c.ReadDB.Host = readDBHost
+
+	readDBName, ok := os.LookupEnv("READ_DB_NAME")
+	if !ok {
+		return errors.New("READ_DB_NAME env is not loaded")
+	}
+
+	// set ReadDBName
+	c.ReadDB.Name = readDBName
+
+	readDBUser, ok := os.LookupEnv("READ_DB_USER")
+	if !ok {
+		return errors.New("READ_DB_USER env is not loaded")
+	}
+
+	// set ReadDBUser
+	c.ReadDB.User = readDBUser
+
+	readDBPassword, ok := os.LookupEnv("READ_DB_PASSWORD")
+	if !ok {
+		return errors.New("READ_DB_PASSWORD env is not loaded")
+	}
+
+	// set ReadDBPassword
+	c.ReadDB.Password = readDBPassword
+
+	readDBPort, ok := os.LookupEnv("READ_DB_PORT")
+	if !ok {
+		return errors.New("READ_DB_PORT env is not loaded")
+	}
+
+	// set ReadDBPort
+	c.ReadDB.Port, _ = strconv.Atoi(readDBPort)
+
 	return nil
 }
---
+++ config/config_type.go
@@ -0,0 +1,27 @@
+package config
+
+type ConfigWriteDB struct {
+	// Host config
+	Host string
+	// Name config
+	Name string
+	// User config
+	User string
+	// Password config
+	Password string
+	// Port config
+	Port int
+}
+
+type ConfigReadDB struct {
+	// Host config
+	Host string
+	// Name config
+	Name string
+	// User config
+	User string
+	// Password config
+	Password string
+	// Port config
+	Port int
+}
